e030990b9c6adce4ad7bb3de032b3e521a63700e
alexistraen <alexis.traen@gmail.com>
2020-11-22 16:40:00 +0100

    design terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+// display categories without refresh
+
 const homeLogo = document.getElementById("homeLogo"),
     homeBtn = document.getElementById("homeBtn"),
     clothesBtn = document.getElementById("clothesBtn"),
     figuBtn = document.getElementById("figuBtn"),
     caveBtn = document.getElementById("caveBtn"),
+
+    homeFoot = document.getElementById("homeFoot"),
+    clothesFoot = document.getElementById("clothesFoot"),
+    figuFoot = document.getElementById("figuFoot"),
+    caveFoot = document.getElementById("caveFoot"),
 
     home = document.getElementById("home"),
     clothes = document.getElementById("clothes"),
@@ -25,7 +32,6 @@ homeBtn.onclick = function () {
 
 clothesBtn.onclick = function () {
     clothes.style.display = "block";
-
     home.style.display = "none";
     figurine.style.display = "none";
     cave.style.display = "none";
@@ -33,7 +39,6 @@ clothesBtn.onclick = function () {
 
 figuBtn.onclick = function () {
     figurine.style.display = "block";
-
     home.style.display = "none";
     clothes.style.display = "none";
     cave.style.display = "none";
@@ -41,8 +46,37 @@ figuBtn.onclick = function () {
 
 caveBtn.onclick = function () {
     cave.style.display = "block";
-
     home.style.display = "none";
     clothes.style.display = "none";
     figurine.style.display = "none";
 }
+
+homeFoot.onclick = function () {
+    home.style.display = "block";
+    clothes.style.display = "none";
+    figurine.style.display = "none";
+    cave.style.display = "none";
+}
+
+clothesFoot.onclick = function () {
+    clothes.style.display = "block";
+    home.style.display = "none";
+    figurine.style.display = "none";
+    cave.style.display = "none";
+}
+
+figuFoot.onclick = function () {
+    figurine.style.display = "block";
+    home.style.display = "none";
+    clothes.style.display = "none";
+    cave.style.display = "none";
+}
+
+caveFoot.onclick = function () {
+    cave.style.display = "block";
+    home.style.display = "none";
+    clothes.style.display = "none";
+    figurine.style.display = "none";
+}
+
+// end display categories without refresh
